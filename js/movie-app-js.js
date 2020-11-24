00751b0afb339a5cf85416cab3c71097d90cfcca
Evan Mead <evan.c.mead@gmail.com>
2020-11-24 14:36:38 -0600

    getMovies created

--- js/movie-app-js.js
+++ js/movie-app-js.js
@@ -2,3 +2,19 @@
 
 <!--url for Glitch Movie API-->
 const url = "https://absorbed-adaptive-silverfish.glitch.me/movies";
+
+function getMovies(movies) {
+    const url = "https://absorbed-adaptive-silverfish.glitch.me/movies";
+    return fetch(url)
+        .then(result => result.json())
+        .then(console.log)
+        .catch(console.error);
+}
+getMovies().then(console.log);
+
+
+
+function addMovie () {
+
+
+}
